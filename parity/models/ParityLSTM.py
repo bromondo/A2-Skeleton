44bba03e232178e4f1910d632e95336b8ab9225a
Bromondo <edmondomazzulli@live.com>
2026-04-20 13:46:41 -0700

    question 2 started

--- parity/models/ParityLSTM.py
+++ parity/models/ParityLSTM.py
@@ -9,8 +9,8 @@ class ParityLSTM(nn.Module) :
 
     def __init__(self, hidden_dim=16):
         super().__init__()
-        # TODO
+        self.lstm = torch.nn.LSTM(input_size=1,hidden_size=hidden_dim,num_layers=1,bias=True,batch_first=True)
 
 
     def forward(self, x, x_lens):
-        # TODO
+        
