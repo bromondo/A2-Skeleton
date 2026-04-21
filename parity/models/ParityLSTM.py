2a3d6220e741e6a7ec4c73c210850cec0fea438e
Mondo <edmondomazzulli@live.com>
2026-04-20 20:19:01 -0700

    Q2 is done

--- parity/models/ParityLSTM.py
+++ parity/models/ParityLSTM.py
@@ -10,7 +10,11 @@ class ParityLSTM(nn.Module) :
     def __init__(self, hidden_dim=16):
         super().__init__()
         self.lstm = torch.nn.LSTM(input_size=1,hidden_size=hidden_dim,num_layers=1,bias=True,batch_first=True)
-
+        self.fc = torch.nn.Linear(hidden_dim,2)
 
     def forward(self, x, x_lens):
-        
+        out, _ = self.lstm(x)
+        idx = x_lens - 1
+        fn = out[torch.arange(out.size(0)), idx]
+        logits = self.fc(fn)
+        return logits
